6ecc15f24efc1030d080c944abfa05541d5381f4
alberto.blanco <alberto.blanco@bbva.com>
2017-10-04 16:16:42 +0200

    Created new stryker config file specific for Jasmine tests

--- stryker-jasmine.conf.js
+++ stryker-jasmine.conf.js
@@ -2,10 +2,10 @@ module.exports = function(config){
     config.set({
         files: [
             // Add your files here, this is just an example:
-            { pattern: 'src/**/*.js', mutated: true, included: false},
-            'test/mocha/*.js'
+            { pattern: 'src/jasmune/*.js', mutated: true, included: false},
+            'test/**/*.js'
         ],
-        testRunner: 'mocha',
+        testRunner: 'jasmine',
         testFramework: 'mocha',
         coverageAnalysis: 'perTest',
         reporter: ['clear-text', 'progress', 'dots', 'html'],
