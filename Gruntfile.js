5bea375b6daddaf232a32b7c6c5e1dd5da55ce5e
alberto.blanco <alberto.blanco@bbva.com>
2017-10-04 16:15:59 +0200

    Added Jasmine tests to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,20 +8,33 @@ module.exports = function(grunt) {
                 files: {
                     libs: [/* List your library files here */],
                     src: ['src/**/*.js'],
-                    tests: ['test/**/*.js']
+                    tests: ['test/jasmine/*.js']
                 },
                 mutate: {
                     src: ['src/**/*.js']
                 },
                 options: {
-                    testFramework: 'mocha',
+                    testFramework: 'jasmine',
                     testRunner: 'mocha'
                 },
                 plugins: ['stryker-mocha-runner', 'stryker-mocha-framework', 'stryker-jasmine', 'stryker-html-reporter']
             }
+        },
+        jasmine_node: {
+            options: {
+                forceExit: true,
+                match: '.',
+                matchall: false,
+                extensions: 'js',
+                specNameMatcher: 'spec'
+            },
+            all: ['spec/']
         }
     });
 
     grunt.loadNpmTasks('grunt-stryker');
+    grunt.loadNpmTasks('grunt-jasmine-node');
+
     grunt.registerTask('default', ['stryker']);
+    grunt.registerTask('jasmine-test', 'jasmine_node');
 };
